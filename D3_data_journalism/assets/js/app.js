2b947e663090af3b8031ed9cb17f9f2a2b217153
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-07-10 15:39:43 -0500

    Set margins and dimensions. Started graphing.

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,11 @@
-//
+// Set dimensions and margins
+var svgWidth = 960;
+var svgHeight = 600;
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 50
+  };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
